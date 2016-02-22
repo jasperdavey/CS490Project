7a867055d58c4eecf48bcf42ca6324d76e2b03ae
Angelica Llerena <llerena.angelica@gmail.com>
2016-02-21 19:40:51 -0500

    Update midLogin.php

--- Controller/midLogin.php
+++ Controller/midLogin.php
@@ -7,24 +7,26 @@ $ch = curl_init("https://cp4.njit.edu/cp/home/login");
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, "user=$user&pass=$pass&uuid=0xACA021");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-$result = rtrim(curl_exec($ch));
+$result = curl_exec($ch);
 curl_close($ch);
 
 $valid = (strpos($result,"loginok.html") !== false);
+
 if ($valid) {
-    echo "Successfully Logged In. </br>";
+	echo "Successfully Logged In to NJIT. </br>";
 } else {
-		echo "Failed. </br>";
-  }
+    	echo "Failed to Login. Please check your username and password. </br>";
+}
 	  
-/********************Still working on...********************************/
-	  /*
+
+
 $ch = curl_init("###"); //Jasper I need your php url
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, "user=$user&pass=$pass");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$result = rtrim(curl_exec($ch));
-echo $result;
+$result = curl_exec($ch);
+//echo $result;
 curl_close($ch);
-	  */
+
+
 ?>
