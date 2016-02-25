828605d8338aec2ad2685a6eca29307e4b01c040
Totaram Ramrattan <tr88@afsconnect1.njit.edu>
2016-02-25 17:18:54 -0500

    mend

---
+++ View/index.php
@@ -0,0 +1,33 @@
+<?php
+require_once ('form_login.php');
+
+// start html
+echo '
+
+<!DOCTYPE html>
+<html>
+<body style="background-color:white">
+<head><title>CS490 Project</title></head>
+
+<div id="header" style="width:100%;height:120px;background-color:red;font-size:36pt;">NJIT</div>
+
+<div id="login-box">
+    <form method="POST" action="">
+        login id:<br>
+        <input type="text" login="user-id" name="ucid"><br>
+        password:<br>
+        <input type="password" login="password" name="pass"><br>
+        <button>Sign In</button>
+    </form>
+</div>
+</body>
+</html>
+';
+// end html
+
+if(isset($_POST['ucid']) && isset($_POST['pass'])){
+    echo login();
+    return;
+}
+
+?>
