3506acdcd8cb6d580bfee0365f893e64eec86ee4
Totaram Ramrattan <tramrattan.0@gmail.com>
2016-02-25 10:23:55 -0500

    mend

---
+++ View/form_login.php
@@ -0,0 +1,17 @@
+<?php
+
+//Getting user and password and entering NJIT Server
+//$info = file_get_contents('php://input');
+session_start();
+$user = $_POST["ucid"];
+$pass = $_POST["pass"];
+$ch = curl_init("https://web.njit.edu/~aml35/CS490/login.php");
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, "user=$user&pass=$pass");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt ($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+
+?>
