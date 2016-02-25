9cbfefcbbd47fa9dbbe5f70dc0e93266f16d84a5
Totaram Ramrattan <tr88@afsconnect1.njit.edu>
2016-02-25 10:55:19 -0500

    updated login

--- View/form_login.php
+++ View/form_login.php
@@ -1,17 +1,20 @@
 <?php
-
 //Getting user and password and entering NJIT Server
 //$info = file_get_contents('php://input');
 session_start();
 $user = $_POST["ucid"];
 $pass = $_POST["pass"];
-$ch = curl_init("https://web.njit.edu/~aml35/CS490/login.php");
+
+$ch = curl_init("https://web.njit.edu/~aml35/login/login.php");
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, "user=$user&pass=$pass");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 curl_setopt ($ch, CURLOPT_FOLLOWLOCATION, 1);
 $result = curl_exec($ch);
+
+echo $result;
+
 curl_close($ch);
 
 ?>
