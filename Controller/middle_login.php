c050728c7b0d1706e902ce12ddfff3446777398e
Angelica Llerena <llerena.angelica@gmail.com>
2016-02-25 16:21:56 -0500

    Update and rename midLogin.php to middle_login.php

--- Controller/middle_login.php
+++ Controller/middle_login.php
@@ -1,15 +1,15 @@
 <?php
 //Getting user and password and entering NJIT Server
-
-//$info = file_get_contents('php://input');
-
+//session_start();
 $user = $_POST["ucid"];
 $pass = $_POST["pass"];
+
+global $json;
 $report = array();
 
 $ch = curl_init("https://cp4.njit.edu/cp/home/login");
 curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, "user=$user&pass=$pass&uuid=0xACA021");
+curl_setopt($ch, CURLOPT_POSTFIELDS,"user=$user&pass=$pass&uuid=0xACA021");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 curl_setopt ($ch, CURLOPT_FOLLOWLOCATION, 1);
@@ -20,35 +20,19 @@ curl_close($ch);
 
 $valid = (strpos($result,"loginok.html") !== false);
 if ($valid) {
-	$report['200'] = "Successfully Logged In to NJIT.";
-	$json = json_encode($report['200']);
+	$report['status'] = "success";
+	$json = json_encode($report);
     echo "$json </br>";
 }else{
-	$report['404'] = "Failed to Login to NJIT Server. Verify username and password.";
-	$json = json_encode($report['404']);
+	$report['status'] = "failed";
+	$json = json_encode($report);
 	echo "$json </br>";
 }
 
-/*
-//Sending results to Devin from NJIT Server
-$D_url = //Devin website;
 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $D_url);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $result);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-
-curl_exec($ch);
-curl_close($ch);
-*/
-
-// Sending user and password to Jasper.
-$data = "user=$user&pass=$pass";
+//Sending user and password to Jasper.
+$data = "user_id=$user&password=$pass";
 $J_url = "https://web.njit.edu/~jmd57/model.php";
-
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $J_url);
 curl_setopt($ch, CURLOPT_POST, 1);
@@ -57,8 +41,10 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 
-curl_exec($ch);
+$DB_results = curl_exec($ch);
 curl_close($ch);
+
+echo "$DB_results";
 
 
 ?>
