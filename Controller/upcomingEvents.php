9f1aa776aa6bec31f7dedf50e1867aa580d0fd45
Angelica Llerena <llerena.angelica@gmail.com>
2016-03-27 15:50:23 -0400

    Update upcomingEvents.php

--- Controller/upcomingEvents.php
+++ Controller/upcomingEvents.php
@@ -1,9 +1,9 @@
 <?php
 /********MIDDLE END**************
 Project:  CS 490 - Group # 2    *
-FileName: upcomingEvents.php	*
-By:       Angelica Llerena		*
-Date:     March 15, 2016.		*
+FileName: upcomingEvents.php	  *
+By:       Angelica Llerena		  *
+Date:     March 15, 2016.		    *
 *********************************/
 
 
