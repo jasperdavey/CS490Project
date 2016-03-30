a67238a8cd06db3f5ca948bcba6ebe258725e6d3
Totaram Ramrattan <tramrattan.0@gmail.com>
2016-03-30 09:38:02 -0400

    splitting up files

--- events/scripts/controllers/login.js
+++ events/scripts/controllers/login.js
@@ -1,41 +1,56 @@
-function signIn() {
-    var user_id = document.forms["login_form"]["user_id"].value;
-    var pass = document.forms["login_form"]["pass"].value;
-    var response;
-    var params = "arg="+"login"+"&user_id="+user_id + "&pass="+pass;
-    var response1, response2;
-
-
-    var url = "php/controller.php";
-
-    if ( (user_id.length > 0) &&  (pass.length > 0) ){
-        resetFields();
-        var XM = new XMLHttpRequest();
-        XM.onreadystatechange=function(){
-            if (XM.readyState==4){
-                if (XM.status==200){
-                    response = XM.responseText;
-                    if(response == 1 ){
-                        console.log("logged in..");
-                    }
-                }
-                else{
-                    alert("An error has occured making the request")
-                }
-            }
-        }
-        XM.open("POST",url,false);
-        XM.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        XM.send(params);
-
-    }
-    else alert("please enter username & password!");
-}
-
+//clear username and password fields
 function resetFields(){
     document.forms["login_form"]["user_id"].value = "" ;
     document.forms["login_form"]["pass"].value = "" ;
 }
-function closeBox(){
-    document.getElementById("info_box").style.visibility="hidden";
+
+// user login script
+function signIn() {
+    var sign_up_page ="views/signup.html";
+    var user_id = document.forms["login_form"]["user_id"].value;
+    var pass = document.forms["login_form"]["pass"].value;
+    var response;
+    var params = "arg="+"login"+"&user_id="+user_id + "&pass="+pass;
+
+    var url = "php/controller.php";
+    if ( (user_id.length > 0) &&  (pass.length > 0) ){
+        resetFields();
+        if(makeRequest(url,params) == 1){
+            window.location.href=sign_up_page;
+        }
+    }
+    else alert("please enter username & password!");
+}
+
+
+//sign up
+function signUp(){
+    var name = document.forms['sign_up_form']['name'].value;
+    var username = document.forms['sign_up_form']['username'].value;
+    var email = document.forms['sign_up_form']['email'].value;
+    var password = document.forms['sign_up_form']['password'].value;
+    console.log(username);
+}
+
+// HTTP request
+
+function makeRequest(url,params){
+    var XM = new XMLHttpRequest();
+    var response;
+    XM.onreadystatechange=function(){
+        if (XM.readyState==4){
+            if (XM.status==200){
+                response = XM.responseText;
+            }
+            else{
+                alert("An error has occured making the request")
+            }
+        }
+    }
+    XM.open("POST",url,false);
+    XM.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    XM.send(params);
+
+    return response;
+
 }
