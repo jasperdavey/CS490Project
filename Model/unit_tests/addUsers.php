23e8ea402d1a03029bcdacd0b3afed29ee8c0ac8
Jasper Micah Davey <jasper.davey@gmail.com>
2016-03-30 18:28:41 -0400

    Added username field to Users table

---
+++ Model/unit_tests/addUsers.php
@@ -0,0 +1,12 @@
+<?php
+
+    url = "http://web.njit.edu/~jmd57/backend.php";
+
+    $eventsArray
+
+    $addUserArray = array( 'email' => 'jasperd92', 'password' => 'randompassword',
+                           'firstname' => 'Jasper', 'lastname' => 'Davey', 'bio' => 'I like CS'
+    );
+
+
+ ?>
