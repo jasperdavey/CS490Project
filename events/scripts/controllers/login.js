165ecca02823eb85a9340f341e06b5f51446db56
Totaram Ramrattan <tramrattan.0@gmail.com>
2016-03-30 09:00:22 -0400

    working on sessions

--- events/scripts/controllers/login.js
+++ events/scripts/controllers/login.js
@@ -1,61 +1,41 @@
-  function signIn() {
+function signIn() {
+    var user_id = document.forms["login_form"]["user_id"].value;
+    var pass = document.forms["login_form"]["pass"].value;
+    var response;
+    var params = "arg="+"login"+"&user_id="+user_id + "&pass="+pass;
+    var response1, response2;
 
-        var ucid = document.forms["login_form"]["ucid"].value;
-        var pass = document.forms["login_form"]["pass"].value;
-        var response;
-        var params = "ucid="+ucid + "&pass="+pass;
-        var response1, response2;
-        var url = "https://web.njit.edu/~tr88/CS490Project/View/form_login2.php";
 
-        if ( (ucid.length > 0) &&  (pass.length > 0) ){
-            var XM = new XMLHttpRequest();
-            XM.onreadystatechange=function(){
-                if (XM.readyState==4){
-                    if (XM.status==200){
-                        document.getElementById("info_box").style.visibility="visible";
-                        response = XM.responseText;
-                        response = response.split(" ");
+    var url = "php/controller.php";
 
-                        response1 = JSON.parse(response[0]);
-                        response2 = JSON.parse(response[1]);
-
-                        if(response1.status == 'failed'){
-                            document.getElementById("status1").innerHTML = "Incorrect username or password!";
-                        }else{
-                            document.getElementById("status1").innerHTML = "Successfully logged in!";
-
-                        }
-
-                        document.getElementById("status2").innerHTML = response2.status;
-
-                        switch (parseInt(response2.status)) {
-                            case 200:
-                                document.getElementById("status2").innerHTML = "Successfully logged in!";
-                                break;
-                            case 304:
-                                document.getElementById("status2").innerHTML = "Incorrect password!";
-                                break;
-                            case 404:
-                                document.getElementById("status2").innerHTML = "User not found!";
-                                break;
-                            default:
-                        }
-
-                    }
-                    else{
-                        alert("An error has occured making the request")
+    if ( (user_id.length > 0) &&  (pass.length > 0) ){
+        resetFields();
+        var XM = new XMLHttpRequest();
+        XM.onreadystatechange=function(){
+            if (XM.readyState==4){
+                if (XM.status==200){
+                    response = XM.responseText;
+                    if(response == 1 ){
+                        console.log("logged in..");
                     }
                 }
+                else{
+                    alert("An error has occured making the request")
+                }
             }
-            XM.open("POST",url,false);
-            XM.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-            XM.send(params);
         }
-        else alert("please enter username & password!");
-    }
+        XM.open("POST",url,false);
+        XM.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        XM.send(params);
 
-    function closeBox(){
-        document.getElementById("info_box").style.visibility="hidden";
-        document.forms["login_form"]["ucid"].value = "" ;
-        document.forms["login_form"]["pass"].value = "" ;
     }
+    else alert("please enter username & password!");
+}
+
+function resetFields(){
+    document.forms["login_form"]["user_id"].value = "" ;
+    document.forms["login_form"]["pass"].value = "" ;
+}
+function closeBox(){
+    document.getElementById("info_box").style.visibility="hidden";
+}
