76be2b0fa61c9bf593489ad7afc6f5653c64bf3c
Jasper Micah Davey <jasper.davey@gmail.com>
2016-03-30 18:39:52 -0400

    added test for adding user

--- Model/unit_tests/addUsers.php
+++ Model/unit_tests/addUsers.php
@@ -1,12 +1,33 @@
 <?php
 
-    url = "http://web.njit.edu/~jmd57/backend.php";
 
-    $eventsArray
+
+    $url = "http://web.njit.edu/~jmd57/backend.php";
+
+    $events = array( 'ACM', 'IEEE', 'NJIT Robotics Club', 'AlcHE' );
 
     $addUserArray = array( 'email' => 'jasperd92', 'password' => 'randompassword',
-                           'firstname' => 'Jasper', 'lastname' => 'Davey', 'bio' => 'I like CS'
+                           'firstname' => 'Jasper', 'lastname' => 'Davey', 'bio' => 'I like CS',
+                           'events' => $events
     );
 
+    $json_array = json_encode( $addUserArray );
+
+    requestBackend( );
+
+    function requestBackend( )
+    {
+        // Return Results
+
+    	$ch = curl_init();
+    	curl_setopt( $ch, CURLOPT_URL, $url );
+    	curl_setopt( $ch, CURLOPT_POST, 1 );
+    	curl_setopt( $ch, CURLOPT_POSTFIELDS, $json_array );
+    	curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
+    	curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1 );
+    	curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, 0 );
+    	curl_exec( $ch );
+    	curl_close( $ch );
+    }
 
  ?>
