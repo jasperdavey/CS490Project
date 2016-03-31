0ead0b28668f8a28f0748c22e3fea980de53478b
Angelica Llerena <llerena.angelica@gmail.com>
2016-03-31 17:18:01 -0400

    Update createEvent.php

--- Controller/createEvent.php
+++ Controller/createEvent.php
@@ -7,43 +7,40 @@ Date:     March 15, 2016.		*
 *********************************/
 
 //Getting the new event information to send Jasper
-$title = $info['event_title'];
-$host = $info['event_host'];
-$description = $info['event_description'];
-$date = $info['event_date'];
-$time = $info['event_time'];
-$location = $info['event_location'];
-$phone = $info['event_phone'];
-$email = $info['event_email'];
+$info['name']=$_POST['name'];
+$info['bio']=$_POST['bio'];
+$info['dateAndTime']=$_POST['dateAndTime'];
+$info['location']=$_POST['location'];
+$info['image']=$_POST['image'];
 
 //Sending info to Jasper
-$dateAndTime="date=$date&time=$time";
+$data = json_encode($info);
 
-$data = "name=$title&
-		 host=$host&
-		 bio=$description&
-		 dateAndTime=$dateAndTime&
-		 location=$location&
-		 phone=$phone&
-		 email=$email";
-
-echo $data;	 
-		 
-/*
-//$J_url = "https://web.njit.edu/~jmd57/.......";
+$J_url = "https://web.njit.edu/~jmd57/backend.php";
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $J_url);
 curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+curl_setopt($ch, CURLOPT_POSTFIELDS, "json=".$data);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 
 
-$results = curl_exec($ch);
+$DB_results = curl_exec($ch);
 curl_close($ch);
 
-echo "$results";
+$json = json_decode($DB_results,true);
 
-*/
+if($json['status']==200){
+	$_SESSION['name']=$info['name'];
+	$_SESSION['bio']=$info['bio'];
+	$_SESSION['dateAndTime']=$info['dateAndTime'];
+	$_SESSION['location']=$info['location'];
+	$_SESSION['image']=$info['image'];
+	echo $json['status'];
+}else{
+	session_destroy();
+	echo $json['status'];
+}
+
 ?>
