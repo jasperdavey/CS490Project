1e8b0c5b4dc1b5847236fede54bb5f5d3d7b7658
Jasper Micah Davey <jasper.davey@gmail.com>
2016-03-31 00:53:04 -0400

    debugging

--- Model/BackendAPI/createEvent.php
+++ Model/BackendAPI/createEvent.php
@@ -19,12 +19,12 @@
         reportBack( $status );
     }
 
+    $id = mysql_insert_id( );
     // Save tags. Assuming nice values given = 0 at this point
     foreach ( $result->tags as $tag => $nice )
     {
-        echo "HERE";
         $sql = sprintf( "INSERT INTO Tags ( id, tag, nice, type )
-                         VALUES ('%s', '%s', '%s', '%s' )", mysql_real_escape_string( mysql_insert_id( ) ),
+                         VALUES ('%s', '%s', '%s', '%s' )", mysql_real_escape_string( $id ),
                          mysql_real_escape_string( $tag ), mysql_real_escape_string( $nice ),
                          mysql_real_escape_string( 1 )
         );
