5509a5458fc8e5bef4682652e9faaf16fdec7a3b
Jasper Micah Davey <jasper.davey@gmail.com>
2016-03-31 00:22:53 -0400

    debugging

--- Model/unit_tests/addUsers.php
+++ Model/unit_tests/addUsers.php
@@ -5,10 +5,11 @@
     $url = "https://web.njit.edu/~jmd57/backend.php";
 
     $events = array( 'ACM', 'IEEE', 'NJIT Robotics Club', 'AlcHE' );
+    $tags = array( '#CS' => 2, '#ComputerScience' => 5, '#NJIT' => 7 );
 
     $addUserArray = array( 'command' => 1, 'email' => 'jasperd92', 'password' => 'randompassword',
                            'firstname' => 'Jasper', 'lastname' => 'Davey', 'bio' => 'I like CS',
-                           'events' => $events
+                           'events' => $events, 'tags' => json_encode( $tags )
     );
 
     $json_array = json_encode( $addUserArray );
