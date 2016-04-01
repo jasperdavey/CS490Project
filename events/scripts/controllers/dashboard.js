f9c254f45d508a02701bdf481f2c5d455f51cb85
Totaram Ramratta <tramrattan@forbes.com>
2016-04-01 11:54:54 -0400

    creat event form view working

--- events/scripts/controllers/dashboard.js
+++ events/scripts/controllers/dashboard.js
@@ -11,3 +11,20 @@ function closeDashMenu(){
     var y = document.getElementById('dashboard_menu_button');
     y.style.visibility="visible";
 }
+
+function initCreateEvent(){
+   var view = document.getElementById("createEventForm");
+   view.style.visibility = "visible";
+   closeDashMenu();
+}
+
+function cancelEventEntry(){
+   var view = document.getElementById("createEventForm");
+   view.style.visibility = "collapse";
+}
+
+function saveEvent(){
+  var view = document.getElementById("createEventForm");
+  view.style.visibility = "collapse";
+
+}
