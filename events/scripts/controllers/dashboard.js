af0b5f5d4ce11d1765d0a3fd9e741dc31c0e898e
Totaram Ramratta <tramrattan@forbes.com>
2016-04-01 10:39:01 -0400

    added menu fun dashboard

--- events/scripts/controllers/dashboard.js
+++ events/scripts/controllers/dashboard.js
@@ -1,9 +1,13 @@
 function showDashMenu(){
     var x = document.getElementById('dashboard_menu');
+    var y = document.getElementById('dashboard_menu_button');
+    y.style.visibility="collapse";
     x.style.visibility="visible";
 }
 
 function closeDashMenu(){
     var x = document.getElementById('dashboard_menu');
     x.style.visibility="collapse";
+    var y = document.getElementById('dashboard_menu_button');
+    y.style.visibility="visible";
 }
