3f3fe1f546b2c8928295b52d89f23d43251198b8
Totaram Ramrattan <tramrattan.0@gmail.com>
2016-04-02 19:55:54 -0400

    user info page working

--- events/scripts/controllers/profile_creater.js
+++ events/scripts/controllers/profile_creater.js
@@ -1,9 +1,10 @@
 var selectCount=0;
 var bio= null;
-var userTags= null;
+var userTags;
 
 function displayHashTags(){
   selectCount = 0;
+  userTags = new Set([]);
   var container = document.getElementById("tag_selection");
   var tags = ['computer science', 'hackaton', 'robotics', 'ieee', 'baseball', 'canoe','test','test'];
   var tag;
@@ -29,11 +30,55 @@ function addHashTag(parent,tag){
   newTag.style.height="35px";
   newTag.style.float="left"
   newTag.innerHTML = tagName;
-  newTag.onclick = function(){removeThySelf(container);};
+  newTag.onclick = function(){removeThySelf(container,newTag);};
   container.appendChild(newTag);
+  userTags.add(tagName);
   parent.removeChild(tag);
 }
 
-function removeThySelf(parent){
-  parent.removeChild(this);
+function removeThySelf(parent,child){
+    var selection_tags = document.getElementById("tag_selection");
+
+    if( confirmDelete(child)){
+
+        parent.removeChild(child);
+        userTags.delete(child.innerHTML);
+        console.log(userTags.size);
+    }
+}
+
+function confirmDelete(node){
+    var yes;
+    var tag = node.innerHTML;
+    if( confirm('remove ' +tag+' ?') == true ){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function confirmUserBio(node){
+    if( confirm('leave bio empty?') == true ){
+        return true;
+    }else{
+        return false;
+    }
+}
+//TODO
+//send user info to back end
+function setUserInfo(){
+    var userBio = document.getElementById('user_bio').value;
+    if( userTags.size <= 0){
+        alert("please select atleast 1 tag");
+        return;
+    }
+    if(userBio.length <= 0 ){
+        if( !confirmUserBio()){
+            return;
+        }
+    }
+    console.log(userBio);
+    userTags.forEach(function(value){
+        console.log(value);
+    })
 }
