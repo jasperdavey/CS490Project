39009736d5100200bcda96e39c609e03fc294b33
Totaram Ramrattan <tramrattan.0@gmail.com>
2016-04-07 17:47:15 -0400

    working on event form

--- events/scripts/controllers/dashboard.js
+++ events/scripts/controllers/dashboard.js
@@ -26,7 +26,7 @@ function cancelEventEntry(){
 function saveEvent(){
   var view = document.getElementById("createEventForm");
   view.style.visibility = "collapse";
-  var response = makeEvent();
+  // var response = makeEvent();
   if( response == 200 ){
       alert("event created!")
   }else{
@@ -41,17 +41,23 @@ function makeEvent(){
     var name = document.forms['event_create_form']['name'].value;
     var image = document.forms['event_create_form']['image'].value;;
     var bio = document.forms['event_create_form']['bio'].value;
-    var dateAndTime = document.forms['event_create_form']['dateAndTime'].value;;
+    var startDate = document.forms['event_create_form']['start_date'].value;
+    var startTime = document.forms['event_create_form']['start_time'].value;
+    var endDate = document.forms['event_create_form']['end_date'].value;
+    var endTime = document.forms['event_create_form']['end_time'].value;
     var location = document.forms['event_create_form']['location'].value;
     var command = 3;
 
     var params = "command="+command
       +"&"+"name="+name
       +"&"+"bio="+bio
-      +"&"+"dateAndTime="+dateAndTime
+      +"&"+'startDateTime='+startDate+' '+startTime+':00'
+      +"&"+'endDateTime='+endDate+' '+endTime+':00'
       +"&"+"location="+location
       +"&"+"image="+image;
-    var response = makeRequest(params);
-     console.log("response: "+response);
-     return response;
+
+    console.log(params);
+    // var response = makeRequest(params);
+    //  console.log("response: "+response);
+    //  return response;
 }
