0d266c4bd2272b2ae4568c7908d3e26e252c2ffe
Jasper Micah Davey <jasper.davey@gmail.com>
2016-04-11 23:29:17 -0400

    Added scripts that will update various attributes of users

---
+++ Model/BackendAPI/removeUserEvent.php
@@ -0,0 +1,56 @@
+<?php
+    // event, id
+    $status = 200;
+    $templist = [ ];
+
+    $sql = sprintf( "SELECT * FROM Users WHERE id = '%s'", mysql_real_escape_string( $result->id ) );
+
+    $user = mysql_query( $sql, $connection );
+
+    while ( !$user )
+    {
+        $message = 'Invalid query: ' . mysql_error() . "\n";
+        $message .= 'Whole query: ' . $sql;
+        print( $message );
+        $status = 404;
+        reportBack( $status );
+    }
+
+    while ( $row = mysql_fetch_assoc( $user ) )
+    {
+        $userEvents = explode( ",", $row[ 'events' ] );
+    }
+
+    foreach ( $userEvents as $singleEvent )
+    {
+        if ( $singleEvent != $result->event )
+        {
+            array_push( $templist, $singleEvent );
+        }
+    }
+
+    $sql = sprintf( "UPDATE Users SET events = '%s' WHERE id = '%s'", mysql_real_escape_string( implode( $templist ) ),
+                     mysql_real_escape_string( $result->id )
+    );
+
+    if ( !mysql_query( $sql, $connection ) )
+    {
+        $message = 'Invalid query: ' . mysql_error( ) . "\n";
+        $message .= 'Whole query: ' . $sql;
+        print( $message );
+        $status = 404;
+        reportBack( $status );
+    }
+
+    reportBack( $status );
+    function reportBack( $status )
+    {
+        // Return Results
+        $status_array = array( 'status' => $status );
+        $status_json = json_encode( $status_array );
+
+        die( "$status_json" );
+    }
+
+
+ ?>
---
+++ Model/BackendAPI/updateUserBio.php
@@ -0,0 +1,29 @@
+<?php
+    // Input: bio, id
+    $status = 200;
+
+    $sql = sprintf( "UPDATE Users SET bio = '%s' WHERE id = '%s'", mysql_real_escape_string( $result->bio ),
+                     mysql_real_escape_string( $result->id )
+    );
+
+    if ( !mysql_query( $sql, $connection ) )
+    {
+        $message = 'Invalid query: ' . mysql_error( ) . "\n";
+		$message .= 'Whole query: ' . $sql;
+		print( $message );
+        $status = 404;
+        reportBack( $status );
+    }
+
+    reportBack( $status );
+    function reportBack( $status )
+    {
+        // Return Results
+        $status_array = array( 'status' => $status );
+        $status_json = json_encode( $status_array );
+
+        die( "$status_json" );
+    }
+
+
+?>
