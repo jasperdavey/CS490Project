200c669cefb45c06ecb5e0b9e6386668af2a9ba1
Jasper Micah Davey <jasper.davey@gmail.com>
2016-04-14 16:43:43 -0400

    Fixed issue to insure friend is deleted from both users

--- Model/BackendAPI/Remove/removeUserFriend.php
+++ Model/BackendAPI/Remove/removeUserFriend.php
@@ -42,6 +42,48 @@
         reportBack( $status );
     }
 
+    $newtemplist = [ ];
+    $newUserFriends = [ ];
+
+    $sql = sprintf( "SELECT * FROM Users WHERE id = '%s'", mysql_real_escape_string( $result->targetID ) );
+
+    $user = mysql_query( $sql, $connection );
+
+    if ( !$user )
+    {
+        $message = 'Invalid query: ' . mysql_error() . "\n";
+        $message .= 'Whole query: ' . $sql;
+        print( $message );
+        $status = 404;
+        reportBack( $status );
+    }
+
+    while ( $row = mysql_fetch_assoc( $user ) )
+    {
+        $newUserFriends = explode( ",", $row[ 'friends' ] );
+    }
+
+    foreach ( $newUserFriends as $singleFriend )
+    {
+        if ( $singleFriend != $result->initiatorID )
+        {
+            array_push( $newtemplist, $singleFriend );
+        }
+    }
+
+    $sql = sprintf( "UPDATE Users SET friends = '%s' WHERE id = '%s'", mysql_real_escape_string( implode( $newtemplist ) ),
+                     mysql_real_escape_string( $result->targetID )
+    );
+
+    if ( !mysql_query( $sql, $connection ) )
+    {
+        $message = 'Invalid query: ' . mysql_error( ) . "\n";
+        $message .= 'Whole query: ' . $sql;
+        print( $message );
+        $status = 404;
+        reportBack( $status );
+    }
+
     reportBack( $status );
     function reportBack( $status )
     {
