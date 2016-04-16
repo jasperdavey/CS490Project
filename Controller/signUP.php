3e7460a603dfd194dc9147cdaf6a82ec3ef3d70c
Angelica Llerena <llerena.angelica@gmail.com>
2016-04-16 11:55:36 -0400

    Update signUP.php

--- Controller/signUP.php
+++ Controller/signUP.php
@@ -7,45 +7,48 @@ Date:     March 15, 2016.		*
 *********************************/
 
 $info['firstname'] = $_POST['firstname'];
-if (!isset($info['firstname'])){
-	$error['status'] = 404;
-	$error['Error'] = "Please enter your firstname";
-	$json = json_encode($error); die($json);
-}
 $info['lastname'] = $_POST['lastname'];
-if (!isset($info['lastname'])){
-	$error['status'] = 404;
-	$error['Error'] = "Please enter your lastname";
-	$json = json_encode($error); die($json);
-}
 $info['username'] = $_POST['username'];
-if (!isset($info['username'])){
-	$error['status'] = 404;
-	$error['Error'] = "Please enter your username";
-	$json = json_encode($error); die($json);
-}
 $info['password'] = $_POST['password'];
-if (!isset($info['password'])){
-	$error['status'] = 404;
-	$error['Error'] = "Please enter your password";
-	$json = json_encode($error); die($json);
-}
 $info['email'] = $_POST['email'];
-if (!isset($info['email'])){
-	$error['status'] = 404;
-	$error['Error'] = "Please enter your email";
-	$json = json_encode($error); die($json);
-}
-
-
-//Getting other user's info...
-
 $info['bio'] = $_POST['bio'];
 $info['image'] = $_POST['image'];
 $info['events'] = $_POST['events'];
 $info['friends'] = $_POST['friends'];
 $info['pendingFriendRequests'] = $_POST['pendingFriendRequests'];
 $info['createdEvents'] = $_POST['createdEvents'];
+
+//CHECKING FOR MISSING FIELDS
+if (!isset($info['firstname'])){
+	$error['status'] = 404;
+	$error['Error'] = "Please enter your firstname";
+	$json = json_encode($error); die($json);
+}
+
+if (!isset($info['lastname'])){
+	$error['status'] = 404;
+	$error['Error'] = "Please enter your lastname";
+	$json = json_encode($error); die($json);
+}
+
+if (!isset($info['username'])){
+	$error['status'] = 404;
+	$error['Error'] = "Please enter your username";
+	$json = json_encode($error); die($json);
+}
+
+if (!isset($info['password'])){
+	$error['status'] = 404;
+	$error['Error'] = "Please enter your password";
+	$json = json_encode($error); die($json);
+}
+
+if (!isset($info['email'])){
+	$error['status'] = 404;
+	$error['Error'] = "Please enter your email";
+	$json = json_encode($error); die($json);
+}
+
 
 
 //Putting all info together to send to Jasper
