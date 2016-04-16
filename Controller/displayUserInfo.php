9d4ce453c2dc0abb6e667e2dba021c97a83fce4d
Angelica Llerena <llerena.angelica@gmail.com>
2016-04-16 11:58:30 -0400

    last version

--- Controller/displayUserInfo.php
+++ Controller/displayUserInfo.php
@@ -1,35 +1,58 @@
 <?php
 /********MIDDLE END**************
 Project:  CS 490 - Group # 2    *
-FileName: displayUserInfo.php	    *
+FileName: displayAllUsers.php	*
 By:       Angelica Llerena		*
 Date:     March 15, 2016.		*
 *********************************/
 
-$info['username'] = $_POST['username'];
 
-$data = json_encode($info);
+function getAllUsers(){
+$table_users = array();
 
-$J_url = "https://web.njit.edu/~jmd57/backend.php";
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $J_url);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, "json=".$data);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+$databaseName = "jmd57";
+$serverName = 'sql.njit.edu';
+$userName = 'jmd57';
+$password = 'owypHuH4g';
 
-
-$DB_results = curl_exec($ch);
-curl_close($ch);
-
-$json = json_decode($DB_results,true);
-
-if($json['status']==200){
-	//echo $json['status'];
-	print_r($json['info']);
-}else{
-	echo $json['status'];
+// create connection
+$connection = mysql_connect( $serverName, $userName, $password);
+if ( !$connection )
+{
+    die(' Could not connect: ' . mysql_error() );
 }
+
+// select database
+if ( !mysql_select_db( $databaseName, $connection ) )
+{
+    die( 'Could not select database' );
+}
+
+$sql = "SELECT * FROM Users;";
+
+$db_table_events = mysql_query($sql,$connection);
+if (!$db_table_events){echo "DB Error: could not query the database"; exit;}
+
+while($row = mysql_fetch_assoc($db_table_events)){
+	//$table_users['id'] = $row['id'];
+	$table_users[$row['id']]['firstname'] = $row['firstname'];
+	$table_users[$row['id']]['lastname'] = $row['lastname'];
+	$table_users[$row['id']]['email']= $row['email'];
+	$table_users[$row['id']]['bio'] = $row['bio'];
+	$table_users[$row['id']]['image'] = $row['image'];
+	$table_users[$row['id']]['events'] = $row['events'];
+	$table_users[$row['id']]['friends'] = $row['friends'];
+	$table_users[$row['id']]['createdEvents'] = $row['createdEvents'];
+	$table_users[$row['id']]['pendingFriendRequests'] = $row['pendingFriendRequests'];
+	
+}
+
+$response = json_encode($table_users);
+return $response;
+}
+
+$json = getAllUsers();
+die($json);
+
 
 ?>
