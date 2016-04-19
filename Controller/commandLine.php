3b9ebc1e31e883c4d3f36cb8d8c4838e1c207e71
Angelica Llerena <llerena.angelica@gmail.com>
2016-04-18 22:46:48 -0400

    Added files via upload

--- Controller/commandLine.php
+++ Controller/commandLine.php
@@ -13,35 +13,35 @@ $cmd = $info['command'];
 //CONSENT
 if ($cmd ==1){include 'signUP.php';}				//DONE
 elseif($cmd==2){include 'login.php';}				//DONE
-elseif($cmd==15){include 'acceptFriendRequest.php';} //need testing 
+//elseif($cmd==15){include 'acceptFriendRequest.php';} //need testing 
 //elseif($cmd==##){include 'rejectFriendRequest.php';} //need testing 
 
 
 
 //CREATE...
 elseif($cmd==3){include 'createEvent.php';}			//DONE
-elseif($cmd==4){include 'createComment.php';}		//TO-DO....
-elseif($cmd==7){include 'createFriendRequest.php';}	//need testing 
+//elseif($cmd==4){include 'createComment.php';}		//TO-DO....
+//elseif($cmd==7){include 'createFriendRequest.php';}	//need testing 
 
 //UPDATES...
-//elseif($cmd==##){include 'updateEventBio.php';}		//need testing
-//elseif($cmd==##){include 'updateEventEndDateTime.php';}		//need testing
-//elseif($cmd==##){include 'updateEventLocation.php';}		//need testing
-//elseif($cmd==##){include 'updateEventName.php';}		//need testing
-//elseif($cmd==##){include 'updateEventStartDateTime.php';}		//need testing
-elseif($cmd==5){include 'updateUserBio.php';}		//need testing
-//elseif($cmd==##){include 'updateUserEmail.php';}		//need testing
-//elseif($cmd==##){include 'updateUserFirstname.php';}		//need testing
-//elseif($cmd==##){include 'updateUserLastname.php';}		//need testing
-//elseif($cmd==##){include 'updateUserPassword';}		//need testing
-elseif($cmd==6){include 'updateUserTag.php';}		//need testing
-//elseif($cmd==##){include 'updateUserUsername.php';}		//need testing
+elseif($cmd==27){include 'updateEventBio.php';}		//DONE
+elseif($cmd==28){include 'updateEventEndDateTime.php';}		//DONE
+elseif($cmd==29){include 'updateEventLocation.php';}		//DONE
+elseif($cmd==22){include 'updateEventName.php';}		//DONE
+elseif($cmd==23){include 'updateEventStartDateTime.php';}		//DONE
+elseif($cmd==5){include 'updateUserBio.php';}		//DONE
+elseif($cmd==21){include 'updateUserEmail.php';}		//DONE
+elseif($cmd==17){include 'updateUserFirstname.php';}		//DONE
+elseif($cmd==18){include 'updateUserLastname.php';}		//DONE
+elseif($cmd==19){include 'updateUserPassword';}		//DONE
+elseif($cmd==6){include 'updateUserTag.php';}		//DONE
+elseif($cmd==20){include 'updateUserUsername.php';}		//DONE
 
 
 
 
 //REMOVE....
-elseif($cmd==14){include 'removeUserTag.php';}		//need testing
+//elseif($cmd==14){include 'removeUserTag.php';}		//need testing
 //elseif($cmd==##){include 'removeEventTag.php';}		//need testing
 //elseif($cmd==##){include 'removeUserEvent.php';}		//need testing
 //elseif($cmd==##){include 'removeUserFriend.php';}		//need testing
@@ -57,7 +57,7 @@ elseif($cmd==16){include 'displayUsers.php';}		//DONE
 //MIDDLE - ONLY
 elseif($cmd==11){include 'displayTags.php';}		//DONE
 elseif($cmd==12){include 'search.php';}				//DONE
-elseif($cmd==13){include 'futureEventsWithFriends.php';}	//NEED TESTING
+elseif($cmd==13){include 'futureEventsWithFriends.php';}	//DONE
 elseif($cmd==24){include 'displayEvents.php';}		//DONE
 elseif($cmd==25){include 'futureEvents.php';}		//DONE
 //elseif($cmd==26){include 'userPastEvents.php';}		//TO-DO...
