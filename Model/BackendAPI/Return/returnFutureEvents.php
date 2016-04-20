7c35190c69a490504c371970119856591f0523fb
Jasper Micah Davey <jasper.davey@gmail.com>
2016-04-20 14:28:58 -0400

    testing past and future events query

--- Model/BackendAPI/Return/returnFutureEvents.php
+++ Model/BackendAPI/Return/returnFutureEvents.php
@@ -4,7 +4,7 @@
 
     $today = date("Y-m-d H:i:s");
     echo $today;
-    /*
+
     $sql = sprintf( "SELECT * FROM Events WHERE startDateTime >= '%s'", mysql_real_escape_string( $today ) );
 
     $allEvents = mysql_query( $sql, $connection );
@@ -33,6 +33,5 @@
 
         die( "$status_json" );
     }
-    */
 
  ?>
