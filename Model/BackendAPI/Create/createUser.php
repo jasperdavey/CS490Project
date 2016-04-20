7963b5111c0930d9726c13c4dbc3eae2cb79abbf
Jasper Micah Davey <jasper.davey@gmail.com>
2016-04-20 13:44:33 -0400

    On issue #41 . Setting attribute values to  or empty array caused the PHP page to not load. Attempting to remove attributes all together as mentioned in last commit.

--- Model/BackendAPI/Create/createUser.php
+++ Model/BackendAPI/Create/createUser.php
@@ -30,13 +30,11 @@
 
     $events = implode( ",", $result->events );
 
-    $sql = sprintf( "INSERT INTO Users ( email, password, username, firstname, lastname, bio, image, events, friends, pendingFriendRequests, createdEvents )
-            VALUES ( '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s' )", mysql_real_escape_string( $result->email ),
+    $sql = sprintf( "INSERT INTO Users ( email, password, username, firstname, lastname, bio, image )
+            VALUES ( '%s', '%s', '%s', '%s', '%s', '%s', '%s' )", mysql_real_escape_string( $result->email ),
             mysql_real_escape_string( $result->password ), mysql_real_escape_string( $result->username ),
             mysql_real_escape_string( $result->firstname ), mysql_real_escape_string( $result->lastname ),
-            mysql_real_escape_string( $result->bio ), mysql_real_escape_string( $defaultImage ),
-            mysql_real_escape_string( $events ), mysql_real_escape_string( {} ), mysql_real_escape_string( {} ),
-            mysql_real_escape_string( {} )
+            mysql_real_escape_string( $result->bio ), mysql_real_escape_string( $defaultImage )
      );
 
     if ( !mysql_query( $sql, $connection ) )
--- Model/unit_tests/testUser.sh
+++ Model/unit_tests/testUser.sh
@@ -2,9 +2,10 @@
 # This test creates two users
 url="https://web.njit.edu/~jmd57/backend.php"
 
-result=`curl --request POST $url --data 'json={"command":1,"email":"jmd57@njit.edu","password":"randompassword","username":"jaspy123","firstname":"Jasper","lastname":"Davey","bio":"I like CS","events":{},"tags":{}}'`
+result=`curl --request POST $url --data 'json={"command":1,"email":"test@rutgers.edu","password":"randompassword","username":"jaspy123","firstname":"Jasper","lastname":"Davey","bio":"I like CS","events":{},"tags":{}}'`
 if ! `echo "${result}" | grep -q "200"`; then
     echo "ERROR: could not create user | $0"
+    echo "${result}"
     exit 1
 fi
 
