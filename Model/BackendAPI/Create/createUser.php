cdd3470f693555944f77437c993f3b1df6baf330
Jasper Micah Davey <jasper.davey@gmail.com>
2016-04-21 16:25:48 -0400

    Investigating issue #51. A potential side effect of initiating empty fields with empty string is that the database would consider the field nonempty. This will result in  not working properly with comma separator. Attempting with null

--- Model/BackendAPI/Create/createUser.php
+++ Model/BackendAPI/Create/createUser.php
@@ -29,7 +29,7 @@
     }
 
     $events = implode( ",", $result->events );
-
+    /*
     $sql = sprintf( "INSERT INTO Users ( email, password, username, firstname, lastname, bio, image, events, friends, pendingFriendRequests, createdEvents )
             VALUES ( '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s' )", mysql_real_escape_string( $result->email ),
             mysql_real_escape_string( $result->password ), mysql_real_escape_string( $result->username ),
@@ -37,6 +37,13 @@
             mysql_real_escape_string( $result->bio ), mysql_real_escape_string( $defaultImage ), mysql_real_escape_string( "" ),
             mysql_real_escape_string( "" ), mysql_real_escape_string( "" ), mysql_real_escape_string( "" )
      );
+     */
+     $sql = sprintf( "INSERT INTO Users ( email, password, username, firstname, lastname, bio, image )
+             VALUES ( '%s', '%s', '%s', '%s', '%s', '%s', '%s' )", mysql_real_escape_string( $result->email ),
+             mysql_real_escape_string( $result->password ), mysql_real_escape_string( $result->username ),
+             mysql_real_escape_string( $result->firstname ), mysql_real_escape_string( $result->lastname ),
+             mysql_real_escape_string( $result->bio ), mysql_real_escape_string( $defaultImage )
+      );
 
     if ( !mysql_query( $sql, $connection ) )
     {
