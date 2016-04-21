6aff73c226830170732ca0872e3c3ff2bde97195
Angelica Llerena <llerena.angelica@gmail.com>
2016-04-21 18:33:29 -0400

    Added files via upload

--- Controller/commandLine.php
+++ Controller/commandLine.php
@@ -44,68 +44,115 @@ Date:     March 15, 2016.		*
 35. rejectFriendRequest.php
 */
 
-
-
-session_start();
-
 $info['command'] = $_POST["command"]; // sent post information
 $cmd = $info['command'];
-
-
+//echo $cmd;
+//echo "Welcome to Controller";
 if ($cmd ==1){include 'signUP.php';}				//DONE
 elseif($cmd==2){include 'login.php';}				//DONE
-//elseif($cmd==15){include 'acceptFriendRequest.php';} //working on 
-//elseif($cmd==##){include 'rejectFriendRequest.php';} //working on  
-
-
-
-//CREATE...
 elseif($cmd==3){include 'createEvent.php';}			//DONE
-//elseif($cmd==4){include 'createComment.php';}		//TO-DO....
-elseif($cmd==7){include 'createFriendRequest.php';}	//DONE   7
-
-//UPDATES...
-elseif($cmd==27){include 'updateEventBio.php';}		//DONE 
-elseif($cmd==28){include 'updateEventEndDateTime.php';}		//DONE  26
-elseif($cmd==29){include 'updateEventLocation.php';}		//DONE
-elseif($cmd==22){include 'updateEventName.php';}		//DONE
-elseif($cmd==23){include 'updateEventStartDateTime.php';}		//DONE  25
+elseif($cmd==4){include 'createComment.php';}		//TO-DO....
 elseif($cmd==5){include 'updateUserBio.php';}		//DONE  5
-elseif($cmd==21){include 'updateUserEmail.php';}		//DONE  24
-elseif($cmd==17){include 'updateUserFirstname.php';}		//DONE  22
-elseif($cmd==18){include 'updateUserLastname.php';}		//DONE  23
-elseif($cmd==21){include 'updateUserPassword.php';}		//DONE
 elseif($cmd==6){include 'updateUserTag.php';}		//DONE	6
-elseif($cmd==19){include 'updateUserUsername.php';}		//DONE  19
+elseif($cmd==7){
+	//include 'sendFriendRequest.php';
+	$info['initiatorID'] = $_POST['initiatorID'];
+	$info['targetID']= $_POST['targetID'];
 
-// updateUserEvent.php 10
-// updateEventTags.php 17
+	
+	$data = json_encode($info);
+	//echo $data;
+	//Sending to Jasper's url...
+	$J_url = "https://web.njit.edu/~jmd57/backend.php";
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $J_url);
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, "json=".$data);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 
-
-//REMOVE....
-elseif($cmd==14){include 'removeUserTag.php';}		//done
-elseif($cmd==18){include 'removeEventTag.php';}		//need testing
-elseif($cmd==20){include 'removeUserEvent.php';}		//need testing
-//elseif($cmd==##){include 'removeUserFriend.php';}		//working on 
-
-
-
-//RETURN...
+	$DB_results = curl_exec($ch);
+	curl_close($ch);
+	
+	die($DB_results);
+	
+}  
 elseif($cmd==8){include 'recommendEvents.php';}		//DONE  
 elseif($cmd==9){include 'displayUserInfo.php';}		//DONE		
-elseif($cmd==16){include 'displayUsers.php';}		//DONE
-elseif($cmd==30){include 'displayPastEvents.php';}
-elseif($cmd==31){include 'displayFutureEvents.php';}
-elseif($cmd==32){include 'displayEventInfo.php';}
-elseif($cmd==13){include 'displayAllFriendsEvents.php';}
-
-
-//MIDDLE - ONLY
+elseif($cmd==10){include 'updateUserEvent.php';}		//to-do	
 elseif($cmd==11){include 'displayTags.php';}		//DONE
-elseif($cmd==12){include 'search.php';}				//DONE   12
-elseif($cmd==24){include 'displayEvents.php';}		//DONE
-elseif($cmd==25){include 'futureEvents.php';}		//DONE
-//elseif($cmd==26){include 'userPastEvents.php';}		//TO-DO...
-else{echo"Error: 404";}
+elseif($cmd==12){include 'search.php';}				//DONE   
+elseif($cmd==13){include 'displayAllFriendsEvents.php';}
+elseif($cmd==14){include 'removeUserTag.php';}		//done
+elseif($cmd==15){
+	//include 'acceptFriendRequest.php';
+	$info['initiatorID'] = $_POST['initiatorID'];
+	$info['targetID']= $_POST['targetID'];
+
+	
+	$data = json_encode($info);
+	//echo $data;
+	//Sending to Jasper's url...
+	$J_url = "https://web.njit.edu/~jmd57/backend.php";
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $J_url);
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, "json=".$data);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+
+	$DB_results = curl_exec($ch);
+	curl_close($ch);
+
+	die($DB_results);
+} //done
+elseif($cmd==16){include 'displayUsers.php';}		//DONE
+elseif($cmd==17){include 'updateEventTag.php';}		//to do 
+elseif($cmd==18){include 'removeEventTag.php';}		//need testing
+elseif($cmd==19){include 'updateUserUsername.php';}		//DONE  19
+elseif($cmd==20){include 'removeUserEvent.php';}		//need testing
+elseif($cmd==21){include 'updateUserPassword.php';}		//DONE
+elseif($cmd==22){include 'updateUserFirstname.php';}		//DONE  22
+elseif($cmd==23){include 'updateUserLastname.php';}		//DONE  23
+elseif($cmd==24){include 'updateUserEmail.php';}		//DONE  24
+elseif($cmd==25){include 'updateEventStartDateTime.php';}		//DONE  25
+elseif($cmd==26){include 'updateEventEndDateTime.php';}		//DONE  26
+elseif($cmd==27){include 'updateEventName.php';}		//DONE
+elseif($cmd==28){include 'updateEventLocation.php';}		//DONE
+elseif($cmd==29){include 'updateEventBio.php';}		//DONE 
+elseif($cmd==30){include 'displayPastEvents.php';}
+elseif($cmd==31){include 'displayFutureEvents.php';}		//DONE
+elseif($cmd==32){include 'displayEventInfo.php';}
+elseif($cmd==33){include 'displayEvents.php';}		//DONE
+elseif($cmd==34){include 'displayUserFriend.php';}		//DONE.....
+elseif($cmd==35){
+	//include 'rejectFriendRequest.php';
+	$info['initiatorID'] = $_POST['initiatorID'];
+	$info['targetID']= $_POST['targetID'];
+
+	
+	$data = json_encode($info);
+	//echo $data;
+	//Sending to Jasper's url...
+	$J_url = "https://web.njit.edu/~jmd57/backend.php";
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $J_url);
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, "json=".$data);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+
+	$DB_results = curl_exec($ch);
+	curl_close($ch);
+
+	die($DB_results);
+}   
+elseif($cmd==36){include 'removeUserFriend.php';}
+elseif($cmd==37){include 'deleteUser.php';}
+elseif($cmd==38){include 'deleteEvent.php';}
+else{echo"Error: 404 - Controler is not getting the command";}
 
 ?>
