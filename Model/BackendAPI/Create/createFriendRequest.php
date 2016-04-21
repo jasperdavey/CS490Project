c09f014e6623f1162533d91cd66d0e0c7b223b68
Jasper Micah Davey <jasper.davey@gmail.com>
2016-04-21 16:37:03 -0400

    Investigating issue #51. My assumptions were again false and a prevalent comma still appears in the list. Attempting to fix this using .

--- Model/BackendAPI/Create/createFriendRequest.php
+++ Model/BackendAPI/Create/createFriendRequest.php
@@ -37,7 +37,7 @@
         array_push( $pendingFriendRequests, $result->initiatorID );
 
         $sql = sprintf( "UPDATE Users SET pendingFriendRequests = '%s'
-                         WHERE id = '%s'", mysql_real_escape_string( implode( ", ", $pendingFriendRequests ) ),
+                         WHERE id = '%s'", mysql_real_escape_string( substr( implode( ",", $pendingFriendRequests ), 1 ) ),
                          mysql_real_escape_string( $result->targetID )
         );
 
--- Model/unit_tests/testAddUserFriends.sh
+++ Model/unit_tests/testAddUserFriends.sh
@@ -1,7 +1,7 @@
 # Author: Jasper Davey
 url="https://web.njit.edu/~jmd57/backend.php"
 
-result=`curl --request POST $url --data 'json={"command":7,"targetID":22,"initiatorID":21}'`
+result=`curl --request POST $url --data 'json={"command":7,"targetID":23,"initiatorID":22}'`
 if ! `echo "${result}" | grep -q "200"`; then
     echo "ERROR: could not create friend request | $0"
     exit 1
