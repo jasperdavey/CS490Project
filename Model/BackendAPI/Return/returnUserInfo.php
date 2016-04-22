bd087d37b90ccf6d6c1674707a351152406d301e
Jasper Micah Davey <jasper.davey@gmail.com>
2016-04-21 22:39:56 -0400

    added createdEvents to returnUserInfo.php as requested in issue #58

--- Model/BackendAPI/Return/returnUserInfo.php
+++ Model/BackendAPI/Return/returnUserInfo.php
@@ -50,8 +50,8 @@
         $infoArray = array( 'id' => $row[ 'id' ], 'email' => $row[ 'email' ], 'username' => $row[ 'username' ],
                             'firstname' => $row[ 'firstname' ],
                             'lastname' => $row[ 'lastname' ], 'bio' => $row[ 'bio' ], 'image' => $row[ 'image' ],
-                            'events' => $row[ 'events' ], 'friends' => $row[ 'friends' ], 'pendingFriendRequests' => $row[ 'pendingFriendRequests' ], 
-                            'tags' => $userTags
+                            'events' => $row[ 'events' ], 'friends' => $row[ 'friends' ], 'pendingFriendRequests' => $row[ 'pendingFriendRequests' ],
+                            'createdEvents' => $row[ 'createdEvents' ], 'tags' => $userTags
         );
     }
 
