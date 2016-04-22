8928c77312ece4cc5efc494a7f18e9356097fb2e
Jasper Micah Davey <jasper.davey@gmail.com>
2016-04-21 23:13:18 -0400

    Fixed bug in deleteEvents.php where event was not deleted from owner. This new file can also remove event from attendees events list

--- Model/BackendAPI/Delete/deleteEvent.php
+++ Model/BackendAPI/Delete/deleteEvent.php
@@ -1,8 +1,71 @@
 <?php
     $status = 200;
+    $owner = "";
+    $userCreatedEvents = [ ];
+    $templist = [ ];
+
+    $sql = sprintf( "SELECT * FROM Events WHERE id = '%s'", mysql_real_escape_string( $result->id ) );
+    $event = mysql_query( $sql, $connection );
+
+    if ( !$event )
+    {
+        $message = 'Invalid query: ' . mysql_error( ) . "\n";
+        $message .= 'Whole query: ' . $sql;
+        print( $message );
+        $status = 404;
+        reportBack( $status );
+    }
+
+    while ( $row = mysql_fetch_assoc( $event ) )
+    {
+        $owner = $row[ 'owner' ];
+    }
+
+    $sql = sprintf( "SELECT * FROM Users WHERE id = '%s'", mysql_real_escape_string( $owner ) );
+    $user = mysql_query( $sql, $connection );
+
+    if ( !$user )
+    {
+        $message = 'Invalid query: ' . mysql_error( ) . "\n";
+        $message .= 'Whole query: ' . $sql;
+        print( $message );
+        $status = 404;
+        reportBack( $status );
+    }
+
+    while ( $row = mysql_fetch_assoc( $user ) )
+    {
+        $userCreatedEvents = explode( ",", $row[ 'createdEvents' ] );
+    }
+
+    foreach ( $userCreatedEvents as $singleCreatedEvent )
+    {
+        if ( $singleCreatedEvent != $result->id )
+        {
+            array_push( $templist, $singleCreatedEvent );
+        }
+    }
+
+    $templist = implode( ",", $templist );
+    if ( $templist[ 0 ] == "," )
+    {
+        $templist = substr( $templist, 1 );
+    }
+
+    $sql = sprintf( "UPDATE Users SET createdEvents = '%s' WHERE id = '%s'", mysql_real_escape_string( $templist ),
+                     mysql_real_escape_string( $owner )
+    );
+
+    if ( !mysql_query( $sql, $connection ) )
+    {
+        $message = 'Invalid query: ' . mysql_error( ) . "\n";
+        $message .= 'Whole query: ' . $sql;
+        print( $message );
+        $status = 404;
+        reportBack( $status );
+    }
 
     $sql = sprintf( "DELETE From Events WHERE id = '%s'", mysql_real_escape_string( $result->id ) );
-
     if ( !mysql_query( $sql, $connection ) )
     {
         $message = 'Invalid query: ' . mysql_error( ) . "\n";
--- Model/unit_tests/testReturnUserInfo.sh
+++ Model/unit_tests/testReturnUserInfo.sh
@@ -1,7 +1,7 @@
 # Author: Jasper Davey
 url="https://web.njit.edu/~jmd57/backend.php"
 
-result=`curl --request POST $url --data 'json={"command":9,"id":27}'`
+result=`curl --request POST $url --data 'json={"command":9,"id":1}'`
 if ! `echo "${result}" | grep -q "200"`; then
     echo "ERROR: could not get user info | $0"
     exit 1
