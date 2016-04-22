3fbf1ae8bfddef8b364d369a3c8ed66a5dfcf654
Jasper Micah Davey <jasper.davey@gmail.com>
2016-04-21 23:14:01 -0400

    More fixes to deleteEvent.php

--- Model/BackendAPI/Delete/deleteEvent.php
+++ Model/BackendAPI/Delete/deleteEvent.php
@@ -3,7 +3,10 @@
     $owner = "";
     $userCreatedEvents = [ ];
     $templist = [ ];
+    $attendees = [ ];
+    $events = [ ];
 
+    // Get event details
     $sql = sprintf( "SELECT * FROM Events WHERE id = '%s'", mysql_real_escape_string( $result->id ) );
     $event = mysql_query( $sql, $connection );
 
@@ -16,11 +19,14 @@
         reportBack( $status );
     }
 
+    // Get owner of the event
     while ( $row = mysql_fetch_assoc( $event ) )
     {
         $owner = $row[ 'owner' ];
+        $attendees = explode( ",", $row[ 'attendees' ] );
     }
 
+    // Get the owner's createdEvents list
     $sql = sprintf( "SELECT * FROM Users WHERE id = '%s'", mysql_real_escape_string( $owner ) );
     $user = mysql_query( $sql, $connection );
 
@@ -65,6 +71,57 @@
         reportBack( $status );
     }
 
+    // Remove event from ALL attendees
+    foreach ( $attendees as $singleAttendee )
+    {
+        $newtemplist = [ ];
+        $sql = sprintf( "SELECT * Users WHERE id = '%s'", mysql_real_escape_string( $singleAttendee ) );
+        $user = mysql_query( $sql, $connection );
+
+        if ( !$user )
+        {
+            $message = 'Invalid query: ' . mysql_error( ) . "\n";
+            $message .= 'Whole query: ' . $sql;
+            print( $message );
+            $status = 404;
+            reportBack( $status );
+        }
+
+        while ( $row = mysql_fetch_assoc( $user ) )
+        {
+            $events = explode( ",", $row[ 'events' ] );
+        }
+
+        foreach ( $events as $singleEvent )
+        {
+            if ( $singleEvent != $result->id )
+            {
+                array_push( $newtemplist, $singleEvent );
+            }
+        }
+
+        $newtemplist = implode( ",", $newtemplist );
+        if ( $newtemplist[ 0 ] == "," )
+        {
+            $newtemplist = substr( $newtemplist, 1 );
+        }
+
+        $sql = sprintf( "UPDATE Users SET createdEvents = '%s' WHERE id = '%s'", mysql_real_escape_string( $newtemplist ),
+                         mysql_real_escape_string( $singleAttendee )
+        );
+
+        if ( !mysql_query( $sql, $connection ) )
+        {
+            $message = 'Invalid query: ' . mysql_error( ) . "\n";
+            $message .= 'Whole query: ' . $sql;
+            print( $message );
+            $status = 404;
+            reportBack( $status );
+        }
+    }
+
+
+    // Remove the event
     $sql = sprintf( "DELETE From Events WHERE id = '%s'", mysql_real_escape_string( $result->id ) );
     if ( !mysql_query( $sql, $connection ) )
     {
