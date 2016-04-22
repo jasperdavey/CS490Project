d3bb218ec45440dac6384d90d9af42f92836d257
Angelica Llerena <llerena.angelica@gmail.com>
2016-04-21 23:59:50 -0400

    Added files via upload

---
+++ Controller/UPDATE/updateUserEvent.php
@@ -0,0 +1,34 @@
+<?php
+/********MIDDLE END**************************
+Project:  CS 490 - Group # 2    			*
+FileName: updateUserEvent.php		    	*
+By:       Angelica Llerena					*
+Date:     April 10, 2016.					*
+*********************************************/
+
+//Input: id, event
+//Output: DB_results
+
+
+$info['id'] = $_POST['id'];
+$info['event']= $_POST['event'];
+
+
+	
+	$data = json_encode($info);
+	
+	//Sending to Jasper's url...
+	$J_url = "https://web.njit.edu/~jmd57/backend.php";
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $J_url);
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, "json=".$data);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+
+	$DB_results = curl_exec($ch);
+	curl_close($ch);
+
+die($DB_results);
+?>
