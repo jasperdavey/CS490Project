d7bc05ee718898677b457259b1878a709d6e28c1
Jasper Micah Davey <jasper.davey@gmail.com>
2016-04-22 01:24:35 -0400

    Added feature to change URL of stored images

--- Model/unit_tests/testUpdateUserTag.sh
+++ Model/unit_tests/testUpdateUserTag.sh
@@ -1,7 +1,7 @@
 # Author: Jasper Davey
 url="https://web.njit.edu/~jmd57/backend.php"
 
-result=`curl --request POST $url --data 'json={"command":6,"owner":7,"tag":"#CS"}'`
+result=`curl --request POST $url --data 'json={"command":6,"owner":6,"tag":"#CS"}'`
 if ! `echo "${result}" | grep -q "200"`; then
     echo "ERROR: could not create tag | $0"
     echo "${result}"
@@ -10,7 +10,7 @@ fi
 
 echo "${result}"
 
-result=`curl --request POST $url --data 'json={"command":6,"owner":7,"tag":"#Software"}'`
+result=`curl --request POST $url --data 'json={"command":6,"owner":6,"tag":"#Software"}'`
 if ! `echo "${result}" | grep -q "200"`; then
     echo "ERROR: could not create tag | $0"
     exit 1
